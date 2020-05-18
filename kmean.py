6130c1b65187ebdc9b62fc8055ee1f6f79922847
Haadi <Hadiyoville25@gmail.com>
2020-05-18 16:47:42 +0500

    completed

--- kmean.py
+++ kmean.py
@@ -11,18 +11,20 @@ def getImage():
     return temp
 
 
-def kmeans_algo(k=5, img=None):
+def kmeans_algo(k=3, img=None):
     flag = 0
     kmean = [(0, 0, 0)]*k
     # initialising the random majors
     for i in range(k):
-        kmean[i] = np.random.randint(0, 256, 3)
+        l=np.random.randint(0, 220)
+        m=np.random.randint(0, 200)
+        kmean[i] = img[l][m]
 
 
     while flag != k:
         # this is used to store the point in specific mean category
         points = [[] for i in range(k)]
-
+        pixels = [[] for i in range(k)]
 
         # calc. euclidean distance of each point and
         # place it accordingly in the points list of list
@@ -33,6 +35,7 @@ def kmeans_algo(k=5, img=None):
                     if distance.euclidean(img[i][j], kmean[l]) < distance.euclidean(img[i][j], kmean[temp]):
                         temp = l
                 points[temp].append(img[i][j])
+                pixels[temp].append((i, j))
 
         # avg of the k means
         for i in range(k):
@@ -45,14 +48,25 @@ def kmeans_algo(k=5, img=None):
             if comparison.all():
                 flag+=1
             kmean[i] = avg
-        if flag==k:
+        if flag == k:
             break
-    return kmean
+        else:
+            flag = 0;
+    return kmean, pixels
 
-
+def saveImage(image, experiment, imgnum):
+    cv2.imwrite("D:\\Downloads\\Sem 6\\AI\\Image-Compression-using-kmeans-" + str(experiment) + "_img" + str(imgnum) + ".png", image)
 def __main__():
     temp = getImage()
-    print(kmeans_algo(5, temp))
+    image = [[[0 for _ in range(3)] for _ in range(200)] for _ in range(220)]
+    kmean, pixel = kmeans_algo(3, temp)
+
+    for i in range(3):
+        for j in pixel[i]:
+            image[j[0]][j[1]]=kmean[i]
+
+    saveImage(np.asarray(image),1, 1)
+
     return 0
 
 
